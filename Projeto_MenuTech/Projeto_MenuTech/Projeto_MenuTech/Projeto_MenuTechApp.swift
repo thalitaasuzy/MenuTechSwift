4a04e0cd34d2e9ef11279652990aab8b17b795e0
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-12-01 17:21:32 -0300

    Add files "Editar Cardápio"

---
+++ Projeto_MenuTech/Projeto_MenuTech/Projeto_MenuTech/Projeto_MenuTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_MenuTechApp.swift
+//  Projeto_MenuTech
+//
+//  Created by User on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_MenuTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
